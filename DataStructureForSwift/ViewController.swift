e5ba360170e330f58b6d12755f42b20632f433c0
tongyangsheng <964997115@qq.com>
2019-07-26 09:42:49 +0800

    添加测试代码

--- DataStructureForSwift/ViewController.swift
+++ DataStructureForSwift/ViewController.swift
@@ -13,6 +13,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //---------------------------------栈-------------------------
+        print("------------------------------------栈---------------------------------")
+        var stack = Stack<Any>()
+        print("栈内元素个数：\(stack.count)")
+        stack.capacity = 5
+        stack.push(element: 1)
+        print("入栈元素 1 后，栈顶元素：\(stack.peek()!)")
+        stack.push(element: 2)
+        stack.push(element: 3)
+        stack.push(element: 4)
+        print("继续入栈元素 2 3 4")
+        print("此时栈顶元素为：\(stack.peek()!)")
+        print("出栈一个元素，弹出元素为：\(stack.pop()!)")
+        print("栈顶元素变为：\(stack.peek()!)")
+        print("------------------------------------链表---------------------------------")
+        let linkedNode = LinkedListNode(1)
+        print("节点值为：\(linkedNode.value)")
+        
+        let linkedList = LinkedList<Any>()
+        linkedList.appendToEnd(1)
+        linkedList.appendToEnd(2)
+        linkedList.appendToEnd(3)
+        
+        print("新增三个节点，返回链表长度：\(linkedList.count)")
+        print("首节点为：\(linkedList.firstNode!.value)")
+        print("尾节点为：\(linkedList.lastNode!.value)")
+        print("------------------------------------队列---------------------------------")
+        var queue = Queue<Any>()
+        print("初始队列长度：\(queue.count)")
+        queue.enqueue(element: 1)
+        queue.enqueue(element: 2)
+        queue.enqueue(element: 3)
+        print("入队1，2，3 队列长度：\(queue.count)")
+        print("出队一次，出队元素为：\(queue.dequeue()!)")
+        print("此时队首为：\(queue.front!)")
     }
 }
 
