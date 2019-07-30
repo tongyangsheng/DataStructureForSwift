056dc8a984dbff146f555d36714a7f6c193d77cd
tongyangsheng <964997115@qq.com>
2019-07-30 10:01:17 +0800

    二分查找算法

---
+++ DataStructureForSwift/BinarySearch/BinarySearch.swift
@@ -0,0 +1,36 @@
+//
+//  Binary search.swift
+//  DataStructureForSwift
+//
+//  Created by game98 on 2019/7/30.
+//  Copyright © 2019 game98. All rights reserved.
+//
+
+import Foundation
+
+func BinarySearch (sourceArray: [Int],find: Int) -> Int
+{
+    var lower = 0
+    var higher = sourceArray.count - 1
+    var compareTimes = 0
+    while lower <= higher
+    {
+        var middle = (lower + higher)/2
+        compareTimes += 1
+        print("第\(compareTimes)次比较，中点为\(sourceArray[middle])")
+        if find == sourceArray[middle]
+        {
+            return middle
+        }
+        if find > sourceArray[middle]
+        {
+            lower = middle + 1
+        }
+        if find < sourceArray[middle]
+        {
+            higher = middle - 1
+        }
+    }
+    print("无此值")
+    return -1
+}
